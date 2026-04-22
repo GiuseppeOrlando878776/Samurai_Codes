4d42fc50c6ee116592b6f27c0533c4916e2c2828
Giuseppe Orlando <giuseppe1.orlando@mail.polimi.it>
2026-04-22 19:16:23 +0200

    Minor style modification

--- TwoScale_Capillarity_full_compressible/include/user_bc.hpp
+++ TwoScale_Capillarity_full_compressible/include/user_bc.hpp
@@ -47,35 +47,35 @@ auto Inlet(const Field& Q,
            const typename Field::value_type uy_D,
            const typename Field::value_type alpha_l_D,
            const typename Field::value_type alpha_d_D,
-           const typename Field::value_type z_D) {
-  return[&Q, ux_D, uy_D, alpha_l_D, alpha_d_D, z_D]
+           const typename Field::value_type Sigma_D) {
+  return[&Q, ux_D, uy_D, alpha_l_D, alpha_d_D, Sigma_D]
   (const auto& /*normal*/, const auto& cell_in, const auto& /*coord*/)
   {
     /*--- Pre-fetch some variables used multiple times in order to exploit possible vectorization ---*/
-    const auto m_l = Q[cell_in](Ml_INDEX);
-    const auto m_g = Q[cell_in](Mg_INDEX);
-    const auto m_d = Q[cell_in](Md_INDEX);
+    const auto m_l_loc = Q[cell_in](Ml_INDEX);
+    const auto m_g_loc = Q[cell_in](Mg_INDEX);
+    const auto m_d_loc = Q[cell_in](Md_INDEX);
 
     /*--- Compute phasic pressures form the internal state ---*/
-    const auto rho     = m_l + m_g + m_d;
-    const auto alpha_l = Q[cell_in](RHO_ALPHA_l_INDEX)/rho;
-    const auto alpha_d = alpha_l*m_d/m_l; /*--- TODO: Add a check in case of zero volume fraction ---*/
-    const auto alpha_g = static_cast<typename Field::value_type>(1.0) - alpha_l - alpha_d;
-    const auto rho_liq = (m_l + m_d)/(alpha_l + alpha_d);
-                         /*--- TODO: Add a check in case of zero volume fraction ---*/
-    const auto rho_g   = m_g/alpha_g; /*--- TODO: Add a check in case of zero volume fraction ---*/
+    const auto rho_loc     = m_l_loc + m_g_loc + m_d_loc;
+    const auto alpha_l_loc = Q[cell_in](RHO_ALPHA_l_INDEX)/rho_loc;
+    const auto alpha_d_loc = alpha_l_loc*m_d_loc/m_l_loc; /*--- TODO: Add a check in case of zero volume fraction ---*/
+    const auto alpha_g_loc = static_cast<typename Field::value_type>(1.0) - alpha_l_loc - alpha_d_loc;
+    const auto rho_liq_loc = (m_l_loc + m_d_loc)/(alpha_l_loc + alpha_d_loc);
+                             /*--- TODO: Add a check in case of zero volume fraction ---*/
+    const auto rho_g_loc   = m_g_loc/alpha_g_loc; /*--- TODO: Add a check in case of zero volume fraction ---*/
 
     /*--- Compute the corresponding ghost state ---*/
     xt::xtensor_fixed<typename Field::value_type, xt::xshape<Field::n_comp>> Q_ghost;
     const auto alpha_g_D       = static_cast<typename Field::value_type>(1.0) - alpha_l_D - alpha_d_D;
-    const auto ml_D            = alpha_l_D*rho_liq;
-    Q_ghost[Ml_INDEX]          = ml_D;
-    const auto mg_D            = alpha_g_D*rho_g;
-    Q_ghost[Mg_INDEX]          = mg_D;
-    const auto md_D            = alpha_d_D*rho_liq;
-    Q_ghost[Md_INDEX]          = md_D;
-    const auto rho_D           = ml_D + mg_D + md_D;
-    Q_ghost[RHO_Z_INDEX]       = rho_D*z_D;
+    const auto m_l_D           = alpha_l_D*rho_liq_loc;
+    Q_ghost[Ml_INDEX]          = m_l_D;
+    const auto m_g_D           = alpha_g_D*rho_g_loc;
+    Q_ghost[Mg_INDEX]          = m_g_D;
+    const auto m_d_D           = alpha_d_D*rho_liq_loc;
+    Q_ghost[Md_INDEX]          = m_d_D;
+    const auto rho_D           = m_l_D + m_g_D + m_d_D;
+    Q_ghost[RHO_Z_INDEX]       = Sigma_D*std::cbrt(rho_liq_loc*rho_liq_loc);
     Q_ghost[RHO_ALPHA_l_INDEX] = rho_D*alpha_l_D;
     Q_ghost[RHO_U_INDEX]       = rho_D*ux_D;
     Q_ghost[RHO_U_INDEX + 1]   = rho_D*uy_D;
